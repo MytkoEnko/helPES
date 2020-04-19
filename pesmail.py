e7ff60d4d9ac5d1b70c200862e8fb17b633d5697
Dmytro Aleksieiev <deass@ukr.net>
2020-04-19 13:35:52 +0200

    Add last 30 lines of log to mail body

--- pesmail.py
+++ pesmail.py
@@ -7,11 +7,18 @@ from secrets import sendgrid_api_key
 
 def send_mail(date=datetime.utcnow(),file_path = 'shot/screen_to_mail.png'):
 
+    with open("pes-f.log", 'r') as f:
+        data = f.readlines()
+    tail = data[-30:]
+    body = ''
+    for i in tail:
+        body += i + "<br>"
+
     message = Mail(
         from_email='pes-python@dmytro.pl',
-        to_emails='deass@ukr.net',
+        to_emails='dmytro.aleksieiev@gmail.com',
         subject='PES script hang mail ' + str(date),
-        html_content='<strong>and easy to do anywhere, even with Python</strong> <img src="cid:my_content_id"></img>')
+        html_content=f'<strong>Last 30 lines of pes-f.log</strong> <br> {body} <br> <img src="cid:my_content_id"></img>')
 
     with open(file_path, 'rb') as f:
         data = f.read()
